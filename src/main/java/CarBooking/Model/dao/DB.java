304d21178c6c4fd445fdfc689539404700a1b563
Chiyuan <machiyuan03@gmail.com>
2023-10-12 01:31:52 +1100

    Fix bugs about testing

--- src/main/java/CarBooking/Model/dao/DB.java
+++ src/main/java/CarBooking/Model/dao/DB.java
@@ -3,6 +3,6 @@ import java.sql.*;
 
 public abstract class DB {
     protected String driver = "org.sqlite.JDBC";
-    protected String url = "jdbc:sqlite:D:\\Github repo\\Parking-Management\\ASD_DB.db";
+    protected String url = "jdbc:sqlite:ASD_DB.db";
     protected Connection conn;
 }

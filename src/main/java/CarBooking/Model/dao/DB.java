f47726088688526352614d274826c5de99a8b411
Chiyuan <machiyuan03@gmail.com>
2023-10-14 00:01:52 +1100

    Fix the db error

--- src/main/java/CarBooking/Model/dao/DB.java
+++ src/main/java/CarBooking/Model/dao/DB.java
@@ -3,6 +3,6 @@ import java.sql.*;
 
 public abstract class DB {
     protected String driver = "org.sqlite.JDBC";
-    protected String url = "jdbc:sqlite:ASD_DB.db";
+    protected String url = "jdbc:sqlite::resource:ASD_DB.db";
     protected Connection conn;
 }
